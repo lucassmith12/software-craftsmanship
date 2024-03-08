e2c0b9a6bca8a49cb70089892d0ea8912ddd5f78
Luke Smith <ljs174@case.edu>
2024-03-08 00:57:12 -0500

    Cleaned it up a bit

--- hw7/ShortestRoute.java
+++ hw7/ShortestRoute.java
@@ -9,15 +9,26 @@ import java.util.Objects;
 
 public class ShortestRoute {
 
+	/**
+	 * Method takes a graph and a starting point in that graph and returns the shortest route 
+	 * through the graph that visits every node
+	 * 
+	 * @param start -- Starting node
+	 * @param weights -- Weight chart that defines the distance between connected nodes
+	 * @return
+	 */
 	public static List<Room> shortestRoute(Room start, int[][] weights){
 		Objects.requireNonNull(start);
 
 		List<Room> routeList = new ArrayList<>();
 		
+		//Calculate the weights for every connected subgraph of the graph
 		weightGraph(start, weights);
 
-
+		//Build the route according to the shortest weight paths
 		routeList = routeBuilder(start, weights, routeList);
+		
+		//The last entries in routeList are an unnecessary path back to the start, so we remove them
 		while(routeList.get(routeList.size()-1).neighbors.size()!=1) {
 			routeList.remove(routeList.size()-1);
 		}
@@ -25,7 +36,15 @@ public class ShortestRoute {
 		return routeList;
 
 	}
-
+	
+	/**
+	 * A helper method that builds a List path through the graph
+	 * 
+	 * @param start -- starting node
+	 * @param weights -- weight chart
+	 * @param build -- traversal list through the graph
+	 * @return -- a list representation of a traversal through every node in the graph
+	 */
 	private static List<Room> routeBuilder(Room start, int[][]weights, List<Room> build){
 		build.add(start);
 		start.visited = false;
@@ -48,6 +67,13 @@ public class ShortestRoute {
 		return build;
 	}
 
+	/**
+	 * Helper method that generates the weight of every subgraph of the graph
+	 * as a field of the root node of that subgraph
+	 * 
+	 * @param start -- Starting room
+	 * @param weights -- Weight chart
+	 */
 	private static void weightGraph(Room start, int[][] weights) {
 		start.visited = true;
 
